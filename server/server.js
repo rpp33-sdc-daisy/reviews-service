e3440f42384e82b9ae9c88164320d1579befdeba
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-04-08 12:38:08 -0700

    Imported body-parser. Exporting app for unit testing. Using dotenv for server port. Importing routes file and added middleware pointing to routes file.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+module.exports = app;
