29de92abb06a07243c6d53e97ee040208c57d97f
jakobtruong@gmail.com <jakobtruong@gmail.com>
2022-05-05 16:45:11 -0700

    Brough max pool connections back down to 10

--- server/database/queries.js
+++ server/database/queries.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   host: process.env.PG_HOST,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT
+  port: process.env.PG_PORT,
+  max: 10
 });
 
 pool.on('error', (err, client) => {
